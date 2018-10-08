60facdc705bc5a78ae4384f8d35b662c66e9d280
Kohei Watanabe <watanabe@mi.t.u-tokyo.ac.jp>
2018-10-08 16:57:27 +0900

    スケジューラの処理の追加

--- README.md
+++ README.md
@@ -9,8 +9,11 @@ Python 3.6を使用
 pip install -r requirements.txt 
 ```
 
-
 ## 参考記事
 * [simple tables in a web app using flask and pandas with Python](https://sarahleejane.github.io/learning/python/2015/08/09/simple-tables-in-webapps-using-flask-and-pandas-with-python.html)
 * [tableのソート、フィルタ、ページング、編集ができるjQueryプラグイン「footable.js」を使ってみた感想](http://chinpui.net/?p=6)
 * [footable.jsのバグ](https://github.com/fooplugins/FooTable/issues/687)
+    * footable.jsをこの通りに多少書き直した。
+* [herokuでのスケジュール管理にAPSchedulerを使ってみる](http://www.stockdog.work/entry/2017/04/10/003452)
+* [Chrome headless + seleniumをherokuで定期実行](http://katsulog.tech/regularly-run-chrome-headless-selenium-with-heroku/)
+* [herokuのタイムゾーンを変更したらAPSchedulerが動いた話](http://www.stockdog.work/entry/2017/04/15/020130)
--- web-app/server.py
+++ web-app/server.py
@@ -1,9 +1,36 @@
+import atexit
 import os
+import subprocess
 
 import pandas as pd
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+def crawling_job():
+    print("------ Hulu Crawling Started! ------")
+    subprocess.check_call("python ../crawl/selenium_hulu.py", shell=True)
+    print("------ Hulu Crawling Finished! ------")
+
+    # tmpを外す
+    subprocess.check_call("mv data/hulu_movie_list.txt.tmp data/hulu_movie_list.txt", shell=True)
+
+    print("------ Yahoo-movie Crawling Started! ------")
+    subprocess.check_call("python ../crawl/selenium_yahoo_movie.py", shell=True)
+    print("------ Yahoo-movie Crawling Finished! ------")
+
+    # tmpを外す
+    subprocess.check_call("mv ./data/movie_scores.tsv.tmp ./data/movie_scores.tsv", shell=True)
+
+
+cron = BackgroundScheduler(daemon=True)
+cron.add_job(func=crawling_job, trigger="cron", hour=17, minute=0)
+cron.start()
+
+# Shutdown your cron thread if the web process is stopped
+atexit.register(lambda: cron.shutdown(wait=False))
 
 
 def add_footable_to_pandas_html(text):
@@ -16,7 +43,7 @@ def add_footable_to_pandas_html(text):
 
 @app.route("/")
 def show_tables():
-    movie_df = pd.read_csv("../crawl/movie_scores.tsv", sep="\t", names=["タイトル", "点数", "評価数"], index_col=0)
+    movie_df = pd.read_csv("./data/movie_scores.tsv", sep="\t", names=["タイトル", "点数", "評価数"], index_col=0)
     movie_df = movie_df.sort_values("点数", ascending=False)
     movie_df.reset_index(inplace=True)
     movie_df.index += 1
@@ -25,4 +52,4 @@ def show_tables():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", "5000")
-    app.run(host="0.0.0.0", port=int(port), debug=True)
+    app.run(host="0.0.0.0", port=int(port), debug=True, use_reloader=False)
