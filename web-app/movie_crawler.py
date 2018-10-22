d06df732e6e3d7c8751d40333ff0d6e8911e244d
Kohei Watanabe <watanabe@mi.t.u-tokyo.ac.jp>
2018-10-23 07:55:17 +0900

    クローラーをserver.pyから分離

---
+++ web-app/movie_crawler.py
@@ -0,0 +1,39 @@
+import atexit
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from crawl.db_setting import session
+from crawl.items import Time
+from crawl.selenium_hulu import HuluSelenium
+from crawl.selenium_yahoo_movie import YahooMovieSelenium
+
+
+def crawling_job():
+    print("------ Hulu Crawling Started! ------")
+    hulu_sele = HuluSelenium()
+    hulu_sele.run()
+    print("------ Hulu Crawling Finished! ------")
+
+    print("------ Yahoo-movie Crawling Started! ------")
+    ym_sele = YahooMovieSelenium()
+    ym_sele.run()
+    print("------ Yahoo-movie Crawling Finished! ------")
+
+    # 最後にクローリングした時刻をDBに書き出す
+    time = Time()
+    session.add(time)
+    session.commit()
+
+
+def register_job_to_aspscheduler():
+    cron = BackgroundScheduler(daemon=True)
+    cron.add_job(func=crawling_job, trigger="cron", hour=7, minute=20)
+    # cron.add_job(func=crawling_job, trigger="interval", seconds=10)
+    cron.start()
+
+    # Shutdown your cron thread if the web process is stopped
+    atexit.register(lambda: cron.shutdown(wait=False))
+
+
+if __name__ == "__main__":
+    crawling_job()
--- web-app/server.py
+++ web-app/server.py
@@ -1,48 +1,18 @@
 # coding: utf-8
-import atexit
 import os
 
 import numpy as np
 import pandas as pd
-from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, render_template
 
 from crawl.db_setting import Base
 from crawl.db_setting import ENGINE
 from crawl.db_setting import session
 from crawl.items import Time
-from crawl.selenium_hulu import HuluSelenium
-from crawl.selenium_yahoo_movie import YahooMovieSelenium
 
 app = Flask(__name__)
 
 Base.metadata.create_all(bind=ENGINE, tables=[Time.__table__])
-
-
-def crawling_job():
-    print("------ Hulu Crawling Started! ------")
-    hulu_sele = HuluSelenium()
-    hulu_sele.run()
-    print("------ Hulu Crawling Finished! ------")
-
-    print("------ Yahoo-movie Crawling Started! ------")
-    ym_sele = YahooMovieSelenium()
-    ym_sele.run()
-    print("------ Yahoo-movie Crawling Finished! ------")
-
-    # 最後にクローリングした時刻をDBに書き出す
-    time = Time()
-    session.add(time)
-    session.commit()
-
-
-cron = BackgroundScheduler(daemon=True)
-cron.add_job(func=crawling_job, trigger="cron", hour=7, minute=20)
-# cron.add_job(func=crawling_job, trigger="interval", seconds=10)
-cron.start()
-
-# Shutdown your cron thread if the web process is stopped
-atexit.register(lambda: cron.shutdown(wait=False))
 
 
 def add_footable_to_pandas_html(text):
